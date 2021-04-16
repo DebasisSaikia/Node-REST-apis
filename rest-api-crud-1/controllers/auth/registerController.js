1c8fe3ec3ee82b5339dfb9c9ad4870a43f076244
Debasis Saikia <debasishsaikia136@gmail.com>
2021-04-16 21:18:26 +0530

    middlewares

--- rest-api-crud-1/controllers/auth/registerController.js
+++ rest-api-crud-1/controllers/auth/registerController.js
@@ -1,6 +1,23 @@
+import Joi from 'joi';
+
 const registerController = {
 
-    register(req, res, next) {
+    async register(req, res, next) {
+
+        // validation for users
+        const registerSchema = Joi.object({
+            name: Joi.string().min(3).max(12).required(),
+            email: Joi.string().email().required(),
+            password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
+            confirm_password: Joi.ref('password')
+        })
+
+        const { error } = registerSchema.validate(req.body);
+
+        if (error) {
+            return next(error);
+        }
+
         res.json({ msg: 'Hello from express' })
     }
 }
