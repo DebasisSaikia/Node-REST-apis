10688006e3b7ab68dd6d98f908591bfaa23f14fa
Debasis Saikia <debasishsaikia136@gmail.com>
2021-04-24 22:07:02 +0530

    callback

--- backend-full-course/callsk.js
+++ backend-full-course/callsk.js
@@ -1,10 +1,24 @@
-const bar = () => console.log('bar');
-const baz = () => console.log('baz');
+// const bar = () => console.log('bar');
+// const baz = () => console.log('baz');
 
-const foo = () => {
-    console.log('foo');
-    baz()
-    bar();
+// const foo = () => {
+//     console.log('foo');
+//     baz()
+//     bar();
+// }
+// foo()
+
+
+// =====================================================================
+// Callback ,async and call stack
+function getMessage(msg, callback) {
+    setTimeout(() => {
+        console.log(msg)
+        callback()
+    }, 100)
 }
 
-foo()
+function displayMessage() {
+    console.log('Display Message !!')
+}
+getMessage('Get Message', displayMessage)  //first get message will be executed and then displaymessage will be executed
